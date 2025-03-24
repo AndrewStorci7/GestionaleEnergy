d26844e1fa2acb73c0e190b8420616e636adbaca
Daniele Zeraschi <dzeraschi2005@gmail.com>
2025-03-24 08:47:37 +0100

    Modifiche Aggiuntive

--- gestionale-energy/src/app/components/main/alert.js
+++ gestionale-energy/src/app/components/main/alert.js
@@ -101,36 +101,36 @@ export default function Alert({
     case "error": {
       return(
         <div className="bg-neutral-200/50 w-screen h-screen fixed top-0 left-0 z-3">
-        <div className=""
-        style={{
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          padding: "20px",
-          backgroundColor: "rgba(255, 0, 0, 1)",
-          color: "white",
-          borderRadius: "5px",
-          boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
-          zIndex: 999,
-          textAlign: "center",
-        }}> 
-        <p>Errore: {msg ? msg : message}</p>
-        <button
-        onClick={() => closeAlert()}
-        style={{
-          padding: "5px 10px",
-          backgroundColor: "darkred",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-          marginTop: "10px",
-        }}
-        >
-        Chiudi
-        </button>
-        </div>
+          <div className=""
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              padding: "20px",
+              backgroundColor: "rgba(255, 0, 0, 1)",
+              color: "white",
+              borderRadius: "5px",
+              boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
+              zIndex: 999,
+              textAlign: "center",
+            }}> 
+            <p>Errore: {msg ? msg : message}</p>
+            <button
+              onClick={() => closeAlert()}
+              style={{
+                padding: "5px 10px",
+                backgroundColor: "darkred",
+                color: "white",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+                marginTop: "10px",
+              }}
+            >
+              Chiudi
+            </button>
+          </div>
         </div>
       );
     }        
