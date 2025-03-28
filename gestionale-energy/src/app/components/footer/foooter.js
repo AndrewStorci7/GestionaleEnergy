ba5cb81b8fe4fa71dcc2824194a02943113208e2
Daniele Zeraschi <dzeraschi2005@gmail.com>
2025-03-28 18:01:34 +0100

    Aggiunti nuovi bottoni ricerca,annulla. Modifica Grafica Header/Search/Footer

--- gestionale-energy/src/app/components/footer/foooter.js
+++ gestionale-energy/src/app/components/footer/foooter.js
@@ -6,25 +6,23 @@ import Image from "next/image";
 
 export default function Footer( props ) {
     return (
-        <div className="absolute end-[0] bottom-[0] w-screen h-[50px] h-[20px] bg-primaryON justify-center">
-            <div className="flex justify-center">
-                <div className="col-span-2 p-[5px] ">
-                    <Image
-                            src="/logoon.png"
-                            width={80}
-                            height={100}
-                            alt="Oppimittinetworking Logo"
-                        />
-                </div> 
-                <div className="pt-3 pl-3 font-semibold">
-                Powered by <a href="https://oppimittinetworking.com">Oppimittinetworking.com</a> 
-                </div>
-                <div className="pt-3 pl-3">
-                2025 @‌copyright
-                </div>
-            </div>
-            
-            
+        <div className="on-fix-index p-4 text-slate-500 dark:text-slate-400 rounded-xl mt-12">
+        <div className="flex flex-col sm:flex-row justify-center items-center mt-6">
+          <div className="col-span-2 p-[5px] flex justify-center sm:justify-start">
+            <Image
+              src="/logoon.png"
+              width={60}
+              height={90}
+              alt="Oppimittinetworking Logo"
+            />
+          </div>
+          <div className="pt-3 pl-3 mb-2 font-semibold text-center sm:text-left">
+            Powered by <a href="https://oppimittinetworking.com">Oppimittinetworking.com</a>
+          </div>
+          <div className="pt-3 pl-3 mb-2 text-center sm:text-left">
+            2025 @‌copyright
+          </div>
         </div>
+      </div>
     );
 }
