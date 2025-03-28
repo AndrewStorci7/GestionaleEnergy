4e646e074398a7395b817c2befb53979acd95c8d
Daniele Zeraschi <dzeraschi2005@gmail.com>
2025-03-28 18:29:28 +0100

    Re-Fixed Footer

--- gestionale-energy/src/app/components/footer/foooter.js
+++ gestionale-energy/src/app/components/footer/foooter.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Footer( props ) {
     return (
-<div className="on-fix-index p-4 text-slate-500 dark:text-slate-400 rounded-xl mt-12 footer fixed bottom-0 left-0 w-full bg-white dark:bg-gray-800">
+<div className="on-fix-index p-4 text-slate-500 dark:text-slate-400 rounded-xl mt-12 footer fixed bottom-0 left-0 w-full  dark:bg-gray-800">
   <div className="flex flex-col sm:flex-row justify-center items-center sm:items-start mt-6">
     <div className="col-span-2 p-[5px] mb-4 sm:mb-0 sm:mr-4">
       <Image
