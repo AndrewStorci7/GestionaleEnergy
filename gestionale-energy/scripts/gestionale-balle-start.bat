037d4900f87eb72c55e8fed8b298bca2e876da79
Andrea Storci <laboratorio@oppimittinetworking.com>
2025-08-11 12:11:58 +0200

    Fixed start.bat

--- gestionale-energy/scripts/gestionale-balle-start.bat
+++ gestionale-energy/scripts/gestionale-balle-start.bat
@@ -2,12 +2,7 @@
 
 :: Avvia i due server per il Gestionale Balle di Oppimitti Energy
 
-set "GIT_BASH_PATH=C:\Program Files\Git\bin\bash.exe"
+set "GIT_BASH_PATH=C:\Program Files\Git\git-bash.exe"
 
-cd /d ..
 start "" "%GIT_BASH_PATH%" -c "npm run server:prod"
 start "" "%GIT_BASH_PATH%" -c "npm run start"
-
-@REM cd /d ..
-@REM start cmd /k "npm run server:prod"
-@REM start cmd /k "npm run start"
