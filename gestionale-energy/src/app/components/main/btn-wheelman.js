7cdc3bff30e9e2a249c3c6d0586bc205bf453364
Tark006 <tarik.elatrochi@gmail.com>
2025-09-02 10:35:52 +0200

    Prova commit tarik

--- gestionale-energy/src/app/components/main/btn-wheelman.js
+++ gestionale-energy/src/app/components/main/btn-wheelman.js
@@ -55,7 +55,7 @@ export default function BtnWheelman({
     }
     
     return(
-        <div className="w-1/2 font-bold on-fix-index">
+        <div className="w-1/2 font-bold on-fix-index ">
             <div className="flex flex-row-reverse px-11">
                 <button className="on-btn-wheelman" onClick={() => handleClick("update")}>
                     <div className="flex items-center p-1">
